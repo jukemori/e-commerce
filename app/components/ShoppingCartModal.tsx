c1bd7b2f359b141f6e7c6ff50d2a4fe45c4d4290
Jun Ukemori <jukemori@gmail.com>
2024-10-19 19:21:45 +0900

    add checkout button

--- app/components/ShoppingCartModal.tsx
+++ app/components/ShoppingCartModal.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/components/ui/sheet'
 import { useShoppingCart } from 'use-shopping-cart'
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 export default function ShoppingCartModal() {
   const {
     cartCount,
@@ -15,6 +16,7 @@ export default function ShoppingCartModal() {
     handleCartClick,
     cartDetails,
     removeItem,
+    totalPrice,
   } = useShoppingCart()
   return (
     <Sheet open={shouldDisplayCart} onOpenChange={() => handleCartClick()}>
@@ -70,6 +72,32 @@ export default function ShoppingCartModal() {
                 </>
               )}
             </ul>
+          </div>
+
+          <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <p>Subtotal:</p>
+              <p>${totalPrice}</p>
+            </div>
+            <p className="mt-0 5 text-sm text-gray-500">
+              Shippping and taxes are caluclulated at checkout.
+            </p>
+
+            <div className="mt-6">
+              <Button className="w-full">Checkout</Button>
+            </div>
+
+            <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+              <p>
+                OR{' '}
+                <button
+                  onClick={() => handleCartClick()}
+                  className="font-medium text-primary hover:text-primary/80"
+                >
+                  Continue Shopping
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </SheetContent>
